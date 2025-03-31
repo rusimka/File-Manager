2df6ffc2b8e07adeaa6edf91a0808930e9eb6097
rusimka <rusimkad@gmail.com>
2025-03-31 10:51:11 +0200

    Implemented React Icons, and solve the problem with the icons.

--- src/components/FileItem.tsx
+++ src/components/FileItem.tsx
@@ -2,17 +2,18 @@
 import FileItemProps from "../interface/FileItemProps";
 // @ts-ignore
 import File from "../interface/File";
-import {Card, CardContent, Icon, Typography} from "@mui/material";
+import {Card, CardContent, Typography} from "@mui/material";
+import {FaFileAlt, FaFolder,} from "react-icons/fa";
+
+
+const FolderIcon = FaFolder as React.FC<{ size?: number }>;
+const FileIcon = FaFileAlt as React.FC<{ size?: number }>;
 
 const FileItem: React.FC<FileItemProps> = ({ file }) => {
     return (
         <Card sx={{ width: 150, height: 150, margin: 2, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', cursor: 'pointer' }}>
             <CardContent sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                {file.type === 'folder' ? (
-                    <Icon sx={{ fontSize: 40, color: 'primary.main' }} />
-                ) : (
-                    <Icon sx={{ fontSize: 40, color: 'secondary.main' }} />
-                )}
+                {file.type === "folder" ? <FolderIcon size={24} /> : <FileIcon size={24} />}
                 <Typography variant="body2" sx={{ marginTop: 1, textAlign: 'center' }}>
                     {file.name}
                 </Typography>
